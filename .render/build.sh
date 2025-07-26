d0cb1f9f99ee717570532304c2761da2710eb80a
siva023-cpu <mssk6304445254@gmail.com>
2025-07-26 17:25:19 +0530

    update

--- .render/build.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-pip install -r requirements.txt
-| Key             | Value                                                |
-| --------------- | ---------------------------------------------------- |
-| `FLASK_ENV`     | `production`                                         |
-| `MAIL_USERNAME` | [mssk6304445254@gmail.com](mailto:your_email@gmail.com) |
-| `MAIL_PASSWORD` | Siva@2002(not Gmail pw)                   |
-| `SECRET_KEY`    |         Siva                  |
--- app.py
+++ app.py
@@ -108,6 +108,6 @@ def logout():
     return redirect('/login')
 
 if __name__ == '__main__':
-    with app.app_context():
-        db.create_all()
-    app.run(debug=True)
+    from waitress import serve
+    serve(app, host='0.0.0.0', port=int(os.environ.get('PORT', 5000)))
+
